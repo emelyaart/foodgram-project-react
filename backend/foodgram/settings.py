598c9f8118ad7c0bc6f411fce32d4d9406339398
Anton Emelyanov <emelyaart@gmail.com>
2021-07-23 16:49:40 +0500

    fix setting

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -1,14 +1,10 @@
 import os
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
